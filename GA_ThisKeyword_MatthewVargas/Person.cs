f703a8386cbf4f16f67d7b5ee1813675afe237e2
matterwoven <145516753+matterwoven@users.noreply.github.com>
2024-01-19 16:40:21 -0800

    Add project files.

---
+++ GA_ThisKeyword_MatthewVargas/Person.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GA_ThisKeyword_MatthewVargas
+{
+    internal class Person
+    {
+        private string Name;
+        private int Wagehourly;
+
+        public Person(string name, int wagehourly)
+        {
+            this.Name = name;
+            this.Wagehourly = wagehourly;
+        }
+        public void Info()
+        {
+            Console.WriteLine($"Name: {this.Name} - Wage: ${this.Wagehourly}/hr"); //With multiple string and int values avaliable at once in the same class objects are more adaptable and flexible
+        }
+
+        public static void Test()
+        {
+            Person[] employees = new Person[2];
+
+            employees[0] = new Person("Jesse", 24);
+            employees[1] = new Person("Matthew", 17);
+            
+            foreach (Person employee in employees)
+            {
+                employee.Info();
+            }
+        }
+
+
+
+    }
+}
---
+++ GA_ThisKeyword_MatthewVargas/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Diagnostics.Metrics;
+using System.Security.Cryptography.X509Certificates;
+using System.Xml.Linq;
+
+namespace GA_ThisKeyword_MatthewVargas
+{
+    internal class Program
+    {
+        //Matthew Vargas
+        //1.19.2024
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Checking employee databases, displaying data");
+            Person.Test();
+            Console.WriteLine("----");
+            Console.WriteLine("Thank you for using this service");
+        }
+    }
+}
